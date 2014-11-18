9ae151d42bd25443dd62b71dec7f63c5df3c5b03
David Vail <dcvail17@gmail.com>
2014-11-18 11:59:54 -0500

    Get basic color buffer working. Is OK when run from Cursive REPL, but crashes on lein run.

--- src/clodiku/MyGame.clj
+++ src/clodiku/MyGame.clj
@@ -4,5 +4,8 @@
     :extends com.badlogic.gdx.Game)
   (:import (com.badlogic.gdx Game)))
 
+(require 'clodiku.mainscreen)
+(refer 'clodiku.mainscreen)
+
 (defn -create [^Game this]
-  (.setScreen this (clodiku.mainscreen/screen)))
+  (.setScreen this (screen)))
--- src/clodiku/mainscreen.clj
+++ src/clodiku/mainscreen.clj
@@ -1,11 +1,14 @@
 (ns clodiku.mainscreen
-  (:import (com.badlogic.gdx Gdx Screen)))
-
+  (:import (com.badlogic.gdx Gdx Screen)
+           (com.badlogic.gdx.graphics GL20)))
 
 (defn screen []
   (proxy [Screen] []
     (show [])
-    (render [])
+    (render [delta]
+      (doto (Gdx/gl)
+        (.glClearColor 0 0 0.2 0.3)
+        (.glClear GL20/GL_COLOR_BUFFER_BIT)))
     (dispose[])
     (hide [])
     (pause [])
