c270d009c5ea3a4b62f4b7569d8575ba73b23850
David Vail <dcvail17@gmail.com>
2014-11-20 09:01:00 -0500

    Switch to Brute ECS, enable warn on reflection.

--- project.clj
+++ project.clj
@@ -8,10 +8,12 @@
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.4.1"]
                  [com.badlogicgames.ashley/ashley "1.3.1"]
                  [com.badlogicgames.gdx/gdx-platform "1.4.1"
-                  :classifier "natives-desktop"] ]
+                  :classifier "natives-desktop"]
+                 [brute "0.3.0" :exclusions [org.clojure/clojure]]]
   :repositories [["sonatype"
                   "https://oss.sonatype.org/content/repositories/releases/"]]
   :main clodiku.core
   :aot [clodiku.MyGame]
+  :global-vars {*warn-on-reflection* true}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clodiku/mainscreen.clj
+++ src/clodiku/mainscreen.clj
@@ -1,25 +1,26 @@
 (ns clodiku.mainscreen
   (:import (com.badlogic.gdx Gdx Screen)
            (com.badlogic.gdx.graphics GL20 OrthographicCamera)
-           (com.badlogic.gdx.maps.tiled TmxMapLoader TiledMapRenderer)
+           (com.badlogic.gdx.maps.tiled TmxMapLoader TiledMapRenderer TiledMap)
            (com.badlogic.gdx.graphics.g2d SpriteBatch)
            (com.badlogic.gdx.maps.tiled.renderers OrthogonalTiledMapRenderer)
-           (com.badlogic.ashley.core Engine)))
+           ))
 
 (require '[clodiku.maps.map-core :as maps])
+(require '[brute.entity :as be])
 
 (declare ^OrthographicCamera camera)
 (declare ^SpriteBatch batch)
 (declare ^OrthogonalTiledMapRenderer map-renderer)
 
-(def engine (Engine.))
+(def system (be/create-system))
 
 (defn screen []
   (proxy [Screen] []
     (show []
       (def camera (OrthographicCamera. 400 400) )
       (def batch (SpriteBatch.))
-      (def map-renderer (OrthogonalTiledMapRenderer. (maps/load-map) batch)))
+      (def map-renderer (OrthogonalTiledMapRenderer. (^TiledMap maps/load-map) batch)))
     (render [delta]
       (doto (Gdx/gl)
         (.glClearColor 0 0 0.2 0.3)
